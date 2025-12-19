b4e99c9ee7471ae00ccd93b757212e07c6db4b7e
Francis <franciskarani15@gmail.com>
2025-12-19 22:40:16 +0300

    creates a route to add a school

--- server/app.py
+++ server/app.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, redirect, url_for
 from flask_migrate import Migrate
 from models import db, User, School, UniformApplication, UniformAssignment, Notification
 from dotenv import load_dotenv
+from flask_restful import Api, Resource
 import os
 
 load_dotenv()
@@ -11,6 +12,32 @@ app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db.init_app(app)
 migrate = Migrate(app, db)
+
+api = Api(app)
+
+class Home(Resource):
+    def get(self):
+        return {'message': 'Welcome to the Uniform Management System'}
+
+api.add_resource(Home, '/')
+
+# creates a school account
+class addSchool(Resource):
+    def post(self):
+        data = request.get_json()
+        school = School(
+            name=data['name'],
+            email=data['email'],
+            password=data['password'],
+            location=data['location'],
+            phone_number=data['phone_number'],
+            uniform_types=data['uniform_types']
+        )
+        db.session.add(school)
+        db.session.commit()
+        return school.to_dict(), 201
+    
+api.add_resource(addSchool, '/schools')
 
 
 
--- server/models.py
+++ server/models.py
@@ -26,6 +26,7 @@ class School(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     location = db.Column(db.String(255))
+    contact = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     # Relationships
