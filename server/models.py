ec425d95459f3676339be10ae151d4e76fbfbb5d
Francis <franciskarani15@gmail.com>
2025-12-20 13:46:38 +0300

    creates the first db instance

--- server/models.py
+++ server/models.py
@@ -26,7 +26,7 @@ class School(db.Model, SerializerMixin):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     location = db.Column(db.String(255))
-    phone-number = db.Column(db.String(255))
+    phone_number = db.Column(db.String(255))
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     
     # Relationships
