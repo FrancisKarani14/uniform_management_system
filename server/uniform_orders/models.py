b43fd4b737785763083ff3e79954d3c8f5e18e95
Francis <franciskarani15@gmail.com>
2026-02-04 23:23:50 +0300

    creates a Uniform order model

--- server/uniform_orders/models.py
+++ server/uniform_orders/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+# uniform order model
+class Uniform_order(models.Model):
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('APPROVED', 'Approved'),
+        ('REJECTED', 'Rejected'),
+    ]
+
+    parent = models.ForeignKey('users.ParentProfile', on_delete=models.CASCADE,  related_name='uniform_order')
+    school = models.ForeignKey('scools.School', on_delete=models.CASCADE, related_name='uniform_order')
+    status = models.CharField(
+        max_length=10, choices=STATUS_CHOICES, default='PENDING')
+    
