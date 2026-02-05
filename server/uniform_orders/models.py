bab1222bd4082183dafab0ed0c0b97a9d6a4027b
Francis <franciskarani15@gmail.com>
2026-02-05 10:21:44 +0300

    creates a uniform order model with status and gender choices

--- server/uniform_orders/models.py
+++ server/uniform_orders/models.py
@@ -8,9 +8,26 @@ class Uniform_order(models.Model):
         ('APPROVED', 'Approved'),
         ('REJECTED', 'Rejected'),
     ]
+    GENDER_CHOICES = [
+        ('MALE', 'Male'),
+        ('FEMALE', 'Female'),
+    ]
+
 
     parent = models.ForeignKey('users.ParentProfile', on_delete=models.CASCADE,  related_name='uniform_order')
     school = models.ForeignKey('scools.School', on_delete=models.CASCADE, related_name='uniform_order')
     status = models.CharField(
         max_length=10, choices=STATUS_CHOICES, default='PENDING')
+    gender = models.CharField(max_length=10, choices=GENDER_CHOICES)
+    blouse_size = models.CharField(max_length=10)
+    skirt_size = models.CharField(max_length=10)
+    sweater_size = models.CharField(max_length=10)
+    trouser_size = models.CharField(max_length=10)
+    shoes_size = models.CharField(max_length=10)
+    shirt_size = models.CharField(max_length=10)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+ 
+
+
     
