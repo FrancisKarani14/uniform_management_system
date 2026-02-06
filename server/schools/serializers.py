b3d0ac9c21bc520edd4cdb5883ae2eab6964653d
Francis <franciskarani15@gmail.com>
2026-02-06 12:36:35 +0300

    serializes uniform orders

---
+++ server/schools/serializers.py
@@ -0,0 +1,8 @@
+from rest_framework import serializers
+from .models import Tailor_school_request
+
+# serialize Tailor_school_request model
+class Tailor_school_requestSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tailor_school_request
+        fields = '__all__'
