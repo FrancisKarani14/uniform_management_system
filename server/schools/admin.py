d2aa0e02d663f2f0eef9479c7e717d0a2f36350c
Francis <franciskarani15@gmail.com>
2026-02-06 13:59:37 +0300

    creates and registers a model for parents to apply to a school

--- server/schools/admin.py
+++ server/schools/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import School
+from .models import School, Parent_school_application
 # Register your models here.
 
 admin.site.register(School)
+admin.site.register(Parent_school_application)
--- server/schools/models.py
+++ server/schools/models.py
@@ -12,3 +12,11 @@ class School(models.Model):
 
     def __str__(self):
         return self.name
+
+# create a parent_school_application
+class Parent_school_application(models.Model):
+    parent = models.ForeignKey('users.ParentProfile', on_delete=models.CASCADE, related_name='parent_school_applications')
+    school = models.ForeignKey(School, on_delete=models.CASCADE, related_name='parent_school_applications')
+    student_name = models.CharField(max_length=255)
+    student_age = models.IntegerField()
+    student_gender = models.CharField(max_length=10)
