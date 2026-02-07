b77b3cfe3a558c87e4a78a4aaf2da912ae832a82
Francis <franciskarani15@gmail.com>
2026-02-07 22:19:30 +0300

    corrects school serialization

--- server/schools/serializers.py
+++ server/schools/serializers.py
@@ -2,7 +2,7 @@ from rest_framework import serializers
 from .models import School, Parent_school_application
 
 # serialize Tailor_school_request model
-class Tailor_school_requestSerializer(serializers.ModelSerializer):
+class School_modelSerializer(serializers.ModelSerializer):
     class Meta:
         model = School
         fields = '__all__'
