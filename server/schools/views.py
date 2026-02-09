ae8d679976876001a8a626d8bea126ee274cd979
Francis <franciskarani15@gmail.com>
2026-02-09 11:24:57 +0300

    creates inports to create a viewset

--- server/schools/views.py
+++ server/schools/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import School, Parent_school_application
+from .serializers import School_modelSerializer, Parent_school_application_modelSerializer
 
 # Create your views here.
