8d863fd492c3b02437cb4d6267bd2aaaaf8b0891
Francis <franciskarani15@gmail.com>
2026-02-09 14:02:33 +0300

    imports necessities to create user models viewsets

--- server/users/views.py
+++ server/users/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import User, AdminProfile, TailorProfile, SchoolAdminProfile, ParentProfile, StudentProfile
+from .serializers import UserSerializer, TailorProfileSerializer, ParentProfileSerializer, SchoolAdminProfile, StudentProfileSerializer   
 
 # Create your views here.
