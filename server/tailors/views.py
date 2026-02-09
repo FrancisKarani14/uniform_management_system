7ffeef75504ac85619f880042fd305812d260538
Francis <franciskarani15@gmail.com>
2026-02-09 13:33:01 +0300

    creates a view set for tailor app models

--- server/tailors/views.py
+++ server/tailors/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Tailor_school_request
+from .serializers import Tailor_school_requestSerializer
+
 
 # Create your views here.
+class Tailor_school_requestViewSet(viewsets.ModelViewSet):
+    queryset = Tailor_school_request.objects.all()
+    serializer_class = Tailor_school_requestSerializer
+    # filterset_fields = ['tailor', 'school', 'status']
+
