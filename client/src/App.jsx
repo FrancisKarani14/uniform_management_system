2152a917d87b8b6d0aa9c02267a31922f8fdcee7
Francis <franciskarani15@gmail.com>
2026-02-14 22:13:51 +0300

    removes unwanted imports from app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,7 @@
-import { RouterProvider } from 'react-router';
-import { Toaster } from './components/ui/sonner';
-import { AuthProvider } from './contexts/AuthContext';
-import { AppDataProvider } from './contexts/AppDataContext';
-import { router } from './routes';
-
 export default function App() {
   return (
-    <AuthProvider>
-      <AppDataProvider>
-        <RouterProvider router={router} />
-        <Toaster position="top-right" />
-      </AppDataProvider>
-    </AuthProvider>
+    <div>
+      <h1>Uniform Management System</h1>
+    </div>
   );
 }
