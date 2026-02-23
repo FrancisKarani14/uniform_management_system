4efbfcbee0f45067c6e4e66c01963786abe6cd20
Francis <franciskarani15@gmail.com>
2026-02-23 08:46:52 +0300

    removes username from list display and search fields

--- server/users/admin.py
+++ server/users/admin.py
@@ -7,16 +7,23 @@ from .models import User, AdminProfile, TailorProfile, SchoolAdminProfile, Paren
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
     model = User
-    list_display = ('username', 'email', 'role', 'is_staff', 'is_active',)
+    list_display = ('email', 'role', 'is_staff', 'is_active',)
     list_filter = ('role', 'is_staff', 'is_active',)
-    search_fields = ("username", "email")
-    ordering = ("id",)
-    fieldsets = UserAdmin.fieldsets + (
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Personal info", {"fields": ("first_name", "last_name")}),
+        ("Permissions", {"fields": ("is_active", "is_staff", "is_superuser", "groups", "user_permissions")}),
+        ("Important dates", {"fields": ("last_login", "date_joined")}),
         ("Role Information", {"fields": ("role",)}),
     )
 
-    add_fieldsets = UserAdmin.add_fieldsets + (
-        ("Role Information", {"fields": ("role",)}),
+    add_fieldsets = (
+        (None, {
+            "classes": ("wide",),
+            "fields": ("email", "password1", "password2", "role", "is_staff", "is_active"),
+        }),
     )
 
 admin.site.register(User, CustomUserAdmin)
