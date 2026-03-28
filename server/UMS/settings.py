59b6d97987594393a8a10a5357e6b999679bde7a
Francis <franciskarani15@gmail.com>
2026-03-28 22:52:54 +0300

    adds the supabase connection string as the secret key and prepared the backend for supabase integration

--- server/UMS/settings.py
+++ server/UMS/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 from pathlib import Path
 from datetime import timedelta
 import os
+import dj_database_url
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -26,12 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_mg-@aj*c2!e%t(j&4-l^liq2i*qx0j08-@o+_4ns+sfin0ixx'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'django-insecure-_mg-@aj*c2!e%t(j&4-l^liq2i*qx0j08-@o+_4ns+sfin0ixx')
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', 'True') == 'True'
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
 
 
 # Application definition
@@ -90,10 +90,11 @@ WSGI_APPLICATION = 'UMS.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
 
 
